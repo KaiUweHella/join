72d21a23613f65fe820b3234dc6fe39a49862d2c
Kai <kai.hella27@gmail.com>
2022-05-10 19:07:21 +0200

    update json

--- js/task.js
+++ js/task.js
@@ -2,15 +2,18 @@ let tasks = [
     {
         title : 'Test',
         date : 12355436542514,
+        status : 'backlog', // backlog, todo, in progress, testing, done
         category : 'marketing', // marketing, design, production, sales
         urgency : 'low', // low (green), medium (yellow), high (red)
         description : 'Beschreibung des Tests.',
         user : [{
             name: 'Kai',
             img : 'url',
+            mail : '',
         },{
             name: 'Brett',
             img : 'url',
+            mail : '',
         },]
     }
 ];
@@ -19,11 +22,17 @@ let users = [
     {
         name: 'Kai',
         img : 'url',
+        mail : '',
+        password : '',
     },{
         name: 'Brett',
         img : 'url',
+        mail : '',
+        password : '',
     },{
         name: 'Amar',
         img : 'url',
+        mail : '',
+        password : '',
     }
 ];
