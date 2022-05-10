66af92ac4daf4107adedb6267ecaaabce8e4bc33
Kai <kai.hella27@gmail.com>
2022-05-10 18:56:47 +0200

    edit test array

---
+++ js/task.js
@@ -0,0 +1,29 @@
+let tasks = [
+    {
+        title : 'Test',
+        date : 12355436542514,
+        category : 'marketing', // marketing, design, production, sales
+        urgency : 'low', // low (green), medium (yellow), high (red)
+        description : 'Beschreibung des Tests.',
+        user : [{
+            name: 'Kai',
+            img : 'url',
+        },{
+            name: 'Brett',
+            img : 'url',
+        },]
+    }
+];
+
+let users = [
+    {
+        name: 'Kai',
+        img : 'url',
+    },{
+        name: 'Brett',
+        img : 'url',
+    },{
+        name: 'Amar',
+        img : 'url',
+    }
+];
