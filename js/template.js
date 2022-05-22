694f6c347f9eba8fcfd6183fc346dd83f48cdb45
KaiUweHella <kai.hella27@gmail.com>
2022-05-22 16:07:13 +0200

    add category to task.html

--- js/template.js
+++ js/template.js
@@ -1,7 +1,10 @@
 function cardHTML(tasks, i){
     return /*html*/ `
     <div draggable="true" ondragstart="startDragging(${i})" class="task-each-category">
-        <div class="color-field" style="background-color:var(--color-${tasks.urgency})"></div>
+        <div class="status">
+            <div class="color-field" style="background-color:var(--color-${tasks.urgency})"></div>
+            <div>${tasks.category.toUpperCase()}</div>
+        </div>
         <h4 class="task-h4">${tasks.title}</h4>
          <p class="task-description">${tasks.description}</p>
          <div class="time-and-members">
