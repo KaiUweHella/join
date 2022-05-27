88aa79fa0673b05c38a18bbe6a4ce20a6d9c5e63
Brett Scott <brettsamscott@gmail.com>
2022-05-27 16:28:46 +0200

    clean up / comment add_Task.js, restyle addtask.html + responsive

--- js/add_Task.js
+++ js/add_Task.js
@@ -1,17 +1,20 @@
+//empty array for selected users, from all users dialog
 let selectUser = [];
 
+/**
+ * This function addes the avitar and name to the add task dialog under heading "Assign to" -- to show which uses have been selected
+ */
 function loadImages() {
+  // if the delected user array is not empty ---
   if (selectUser != []) {
     document.getElementById("user-img").innerHTML = "";
     for (let i = 0; i < selectUser.length; i++) {
       let userName = selectUser[i].name;
-      document.getElementById(
-        "user-img"
-      ).innerHTML += `                                    
-      <div style="display: flex; flex-direction: column; align-items: center;">
+      document.getElementById("user-img").innerHTML += `                                    
+      <div class="added-user">
       <img class="avatar" src="./img/avatar.png">
       <span>${userName}</span>
-  </div>`;
+      </div>`;
     }
   }
 }
@@ -19,12 +22,10 @@ function loadImages() {
 // jsdoc: npm install -g jsdoc
 
 /**
- * This function reads input data from addTasks form pushes input to JSON-array then saves this array in localstorage
+ * This function reads input data from addTasks form pushes input to JSON-array then saves this array on backend server
  *
  * @param {string} event - on event here summit button click the default funtcion of the form is prevented so that processes in console can be seen and the page is not automatically reloaded.
  */
-
-
 function addTask(event) {
   // function to prevent the default function of form being carried out
   event.preventDefault();
@@ -35,11 +36,11 @@ function addTask(event) {
   let description = document.getElementById("description");
   let date = document.getElementById("due-date");
   let urgency = document.getElementById("urgency");
-  //   let user = document.getElementById('selectedUser');
+  
 
   createJsonArrayForTask(title, category, description, date, urgency);
   resetFormObjects(title, category, description, date, urgency);
-
+  // task sent confirmation text displayed then turned off after 2 seconds
   document.getElementById(
     "confirm-text"
   ).innerHTML = /*html*/ `Task assigned successfully`;
@@ -47,8 +48,9 @@ function addTask(event) {
   setTimeout(function () {
     document.getElementById("confirm-text").innerHTML = "";
   }, 2000);
-
+  // selected users array emptied
   selectUser = [];
+  //Selected user/s displayed
   loadImages();
 }
 
@@ -62,13 +64,8 @@ function addTask(event) {
  * @param {string} date - due date as string
  * @param {string} urgency task urgency
  */
-
-
-
 function createJsonArrayForTask(title, category, description, date, urgency) {
   // Variables saved as JSON , with .value the value of the form object is returned (writen this way so that the form can then be reset).
-  let description2 = document.getElementById("description").value;
-  console.log("des", description2);
 
   let task = {
     title: title.value,
@@ -77,7 +74,8 @@ function createJsonArrayForTask(title, category, description, date, urgency) {
     date: date.value,
     urgency: urgency.value,
     status: "backlog",
-    user: selectUser,
+    // selected users from array "selectUser"
+    user: selectUser
   };
   // Push JSON to array "tasks" auf server
   tasks.push(task);
@@ -96,15 +94,19 @@ function createJsonArrayForTask(title, category, description, date, urgency) {
  */
 function resetFormObjects(title, category, description, date, urgency) {
   // Reset form objects
+  selectUser = [];
+  //selected user images removed
+  document.getElementById("user-img").innerHTML = "";
   title.value = "";
   category.value = "";
   description.value = "";
   date.value = "";
-  urgency.value = "";
+  urgency = "";
+  
 }
 
 /**
- * This function brings up dialog box with users to assign to the task (From array users in this js file)
+ * This function brings up dialog box with users to assign to the task (From array users on Backend server )
  *
  * @param {string} i - user number
  */
@@ -136,54 +138,42 @@ function showUser(i) {
 }
 
 /**
- * This function adds selected user to textarea in form to be submitted
- *
- * @param {string} i - for testing only
- * @param {string} userName - user name form function "addUser" (div within popup dialogbox)
+ * This Function adds the selected user to the selected users array "selectUser" (from the select user dialog popup function "showUser")
+ * 
+ * @param {string} i - current selection
+ * @returns 
  */
-// function addUser(i, userName) {
-//    document.getElementById('selectedUser').innerHTML += userName;
-//    //add check symbol next to selected user
-//    document.getElementById(`checked_${i}`).innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle svg-check" viewBox="0 0 16 16">
-//    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-//    <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
-//    </svg>`
-//     //closes dialogbox after 500ms
-//    // setTimeout(function(){
-//    // closeUserDialog()
-//    // console.log('useri', i)
-//    // console.log('user', userName)
-//    // }, 500);
-
-// }
 function addUser(i) {
+  // All details of selected user i
   let userInfo = users[i];
+  console.log('userInfo', userInfo)
+  //name of selected user in dialig box
   let userName = users[i].name;
-  //add check symbol next to selected user
-
+  // loop 
   for (let i = 0; i < selectUser.length; i++) {
-    const element = selectUser[i];
-
-    if (userName == element.name) {
+    let selection = selectUser[i];
+    // if statement compares the user in array all users "users" with user in the selected users array "selectUser" if 
+    // user exists warning is logged --- with return we get out of the function.
+    if (userName == selection.name) {
       console.log("User würde schon hinzugefügt");
       return;
     }
   }
-
+  //pushes all details (JSON) for the selected user to array "selectUser" --- 
+  //this will be the user array to be added to the task in function "createJsonArrayForTask"
   selectUser.push(userInfo);
 
   console.log(selectUser);
+
   loadImages();
   showCheckUp();
 
-  //closes dialogbox after 500ms
-  // setTimeout(function(){
-  // closeUserDialog()
-  // console.log('useri', i)
-  // console.log('user', userName)
-  // }, 500);
 }
 
+/**
+ * This function compares the users in all users with the users in selected users if they are the in both then 
+ * a tick will be placed next to the user in the select user dialog popup --- from function "show user"
+ */
 function showCheckUp() {
   for (let i = 0; i < users.length; i++) {
     const userName = users[i].name;
@@ -218,23 +208,4 @@ function setArray(key, array) {
   backend.setItem(key, JSON.stringify(array));
 }
 
-/**
- * This function converts JSON-array to string then saves to local storage
- */
-// function arraySaveToLocalStorage() {
-//     // Json array to string then saved in local storage)
-//      let allTasksAsString = JSON.stringify(tasks);
-//      localStorage.setItem('tasks', allTasksAsString);
-// }
 
-/**
- *  This function reads JSON array as a string from localstorage on page load. Function call in html body "onload"
- *  string is then parsed into JSON array and our allTasks array is overwriten with this array on load.
- */
-// function loadAllTasks() {
-
-//     let allTasksAsString = localStorage.getItem('allTasks');
-//     allTasks = JSON.parse(allTasksAsString);
-
-//     console.log('loaded all Tasks', allTasks);
-// }
