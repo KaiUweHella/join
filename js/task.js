61d433dc12aa6d3042056306a24bcad52ca36c43
KaiUweHella <kai.hella27@gmail.com>
2022-05-28 22:54:25 +0200

    add img to user array || fixed the overview of user at task.html

--- js/task.js
+++ js/task.js
@@ -1,9 +1,7 @@
-
-
-async function showOverview(){
-    await init();
-    showCards();
-  }
+async function showOverview() {
+  await init();
+  showCards();
+}
 
 function showCards() {
   clearOverview();
@@ -11,17 +9,52 @@ function showCards() {
     const task = tasks[i];
     let id = assignStatus(task);
 
-    if(task.status != "backlog"){
+    if (task.status != "backlog") {
       document.getElementById(id).innerHTML += cardHTML(task, i);
+
+      if (task.user.length > 2) {
+        document.getElementById(`number-of-member${i}`).innerHTML =
+          memberNUmberHTML(task.user.length - 2);
+      }
+      if (task.user.length > 0) {
+        loadImgOfMember(task, i);
+      }
     }
   }
 }
 
-function clearOverview(){
-  document.getElementById('inProgress').innerHTML = '';
-  document.getElementById('testing').innerHTML = '';
-  document.getElementById('todo').innerHTML = '';
-  document.getElementById('done').innerHTML = '';
+function loadImgOfMember(task, j) {
+  for (let i = 0; i <= 1; i++) {
+    let user = task.user[i];
+    console.log(user.img);
+    if (user) {
+      document.getElementById(`task-member${j}`).innerHTML += memberHTML(
+        user.img
+      );
+    }
+  }
+}
+
+function showMembers(i) {
+  let task = tasks[i];
+  document.getElementById(`member-overview${i}`).innerHTML = "";
+  document.getElementById(`member-overview${i}`).classList.remove("d-none");
+  for (let j = 0; j < task.user.length; j++) {
+    const user = task.user[j];
+    document.getElementById(`member-overview${i}`).innerHTML +=
+      memberOverviewHTML(user.img);
+  }
+}
+
+function hideMembers(i) {
+  document.getElementById(`member-overview${i}`).classList.add("d-none");
+}
+
+function clearOverview() {
+  document.getElementById("inProgress").innerHTML = "";
+  document.getElementById("testing").innerHTML = "";
+  document.getElementById("todo").innerHTML = "";
+  document.getElementById("done").innerHTML = "";
 }
 
 function assignStatus(element) {
@@ -45,28 +78,28 @@ function assignStatus(element) {
 
 let currentId = [];
 
-function startDragging(i){
- currentId = i;
+function startDragging(i) {
+  currentId = i;
 }
 
 function allowDrop(ev) {
   ev.preventDefault();
 }
 
-function moveTo(category){
-  tasks[currentId]['status'] = category;
+function moveTo(category) {
+  tasks[currentId]["status"] = category;
 
-  setArray('tasks', tasks);
-  
+  setArray("tasks", tasks);
+
   showCards();
 }
 
-function highlight(id){
-  document.getElementById(id).classList.add('drag-erea-highlight');
+function highlight(id) {
+  document.getElementById(id).classList.add("drag-erea-highlight");
 }
 
-function removeHighlight(id){
-  document.getElementById(id).classList.remove('drag-erea-highlight');
+function removeHighlight(id) {
+  document.getElementById(id).classList.remove("drag-erea-highlight");
 }
 
 function setArray(key, array) {
--- js/template.js
+++ js/template.js
@@ -1,8 +1,10 @@
-function cardHTML(tasks, i){
-    return /*html*/ `
+function cardHTML(tasks, i) {
+  return /*html*/ `
     <div draggable="true" ondragstart="startDragging(${i})" class="task-each-category">
         <div class="status">
-            <div class="color-field" style="background-color:var(--color-${tasks.urgency})"></div>
+            <div class="color-field" style="background-color:var(--color-${
+              tasks.urgency
+            })"></div>
             <div>${tasks.category.toUpperCase()}</div>
         </div>
         <h4 class="task-h4">${tasks.title}</h4>
@@ -12,11 +14,36 @@ function cardHTML(tasks, i){
                  <img src="./img/clock.svg" alt="">
                 <p class="date">${tasks.date}</p>
             </div>
-            <div class="task-member">
-                <img class="avatar" src="./img/sergio-de-paula-c_GmwfHBDzk-unsplash.jpg" alt="">
-                <img class="avatar" src="./img/sergio-de-paula-c_GmwfHBDzk-unsplash.jpg" alt="">
+            <div class="task-member" onmouseover="showMembers(${i})" onmouseout="hideMembers(${i})">
+                <div id="task-member${i}">
+                    
+                </div>
+                <div id="number-of-member${i}">
+                    
+                </div>
+                <div class="member-overview d-none" id="member-overview${i}">
+                    
+                </div>
             </div>
         </div>
     </div>
+    `;
+}
+
+function memberNUmberHTML(length){
+    return /*html*/ `
+    <span class="number-of-member">+${length}</span>
+    `;
+}
+
+function memberHTML(img){
+    return /*html*/ `
+        <img class="avatar" src="${img}" alt="">
+    `;
+}   
+
+function memberOverviewHTML(img){
+    return /*html*/ `
+        <img class="avatar-ov" src="${img}" alt="">
     `;
 }
