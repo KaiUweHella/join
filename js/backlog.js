bf2b0dc4d91b32dec65c39ef88ba513825b0e76b
KaiUweHella <kai.hella27@gmail.com>
2022-05-31 19:59:32 +0200

    edit backlog.js

--- js/backlog.js
+++ js/backlog.js
@@ -1,52 +1,49 @@
 let colorsOfUrgency = {
-    low: "#61BD4F",
-    medium: "#F2D600",
-    high: "#EB5A46",
+  low: "#61BD4F",
+  medium: "#F2D600",
+  high: "#EB5A46",
 };
 
 async function showBacklogTask() {
-    await init();
-    renderBacklogTasks();
+  await init();
+  renderBacklogTasks();
 }
 
-
-
 function renderBacklogTasks() {
-    let backlogtable = document.getElementById("backlogTable");
-    backlogtable.innerHTML = "";
-    for (let i = 0; i < tasks.length; i++) {
-        const backlogTask = tasks[i];
-        // const user = users;
-        const colorOfUrgency = colorsOfUrgency[backlogTask["urgency"]];
-        if (backlogTask.status == "backlog") {
-            backlogtable.innerHTML += /*html*/ `
-        <tbody>
-            <tr id="liveAlertBtn"  class="taskButton" onclick="addToBoard(${i})">
-                <td class="priority-con" data-label="Priority" id="urgency-img"  style="color: ${colorOfUrgency}">${tasks[i].urgency}</td>
-                <td data-label="User" class="user-img" id="user"><img src="${users[2].img}" alt="">
-                <span class="">${users[2].name}</span>
-                </td>
-                <td data-label="Mail"><a href="mailto:gabigibsnicht@mail.com">${users[2].mail}</td>
-                <td data-label="Category" id="category">${tasks[i].category}</td>
-                <td data-label="Status" id="status">${tasks[i].status}</td>
-                <td data-label="Details">${tasks[i].description}</td>
-                <div id="liveAlertPlaceholder"></div>
-            </tr>
-        </tbody>`;
+  let backlogtable = document.getElementById("backlogTable");
+  backlogtable.innerHTML = "";
+  for (let i = 0; i < tasks.length; i++) {
+    const backlogTask = tasks[i];
+    // const user = users;
+    const colorOfUrgency = colorsOfUrgency[backlogTask["urgency"]];
+    if (backlogTask.status == "backlog") {
+      backlogtable.innerHTML += backlogTableHTML(i, colorOfUrgency);
 
-        }
+      loadUser(i);
     }
+  }
+}
+
+function loadUser(i) {
+  let task = tasks[i];
+
+  for (let j = 0; j < task.user.length; j++) {
+    const user = task.user[j];
+    document.getElementById(`user-names${i}`).innerHTML += /*html*/ `
+            <img class="user-img"  src="${user.img}" alt="">
+            <span class="">${user.name}</span>
+        `;
+  }
 }
 
 function addToBoard(i) {
-    if (confirm("Aufgabe als todo einstellen")) {
-        tasks[i].status = "todo";
-        setArray("tasks", tasks);
-        renderBacklogTasks();
-    }
+  if (confirm("Aufgabe als todo einstellen")) {
+    tasks[i].status = "todo";
+    setArray("tasks", tasks);
+    renderBacklogTasks();
+  }
 }
 
 function setArray(key, array) {
-    backend.setItem(key, JSON.stringify(array));
+  backend.setItem(key, JSON.stringify(array));
 }
-
