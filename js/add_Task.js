52ffdf0959684d2ef27a9269d36f449a26af7759
Brett Scott <brettsamscott@gmail.com>
2022-06-02 21:53:42 +0200

    feedback fix

--- js/add_Task.js
+++ js/add_Task.js
@@ -48,7 +48,7 @@ function addTask(event) {
 
   setTimeout(function () {
     document.getElementById("confirm-text").innerHTML = "";
-  }, 2000);
+  }, 4000);
   // selected users array emptied
   selectUser = [];
   //Selected user/s displayed
@@ -108,10 +108,8 @@ function resetFormObjects() {
 
 /**
  * This function brings up dialog box with users to assign to the task (From array users on Backend server )
- *
- * @param {string} i - user number
  */
-function showUser(i) {
+function showUser() {
   //clear previous content
   document.getElementById("user-container").innerHTML = "";
   // Remove hidden class
@@ -185,13 +183,19 @@ function showCheckUp() {
       if (userName == selectedUserName) {
         document.getElementById(
           `checked_${i}`
-        ).innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle svg-check" viewBox="0 0 16 16">
+        ).innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" onclick="removeUser(${j, i})" width="16" height="16" fill="currentColor" class="bi bi-check-circle svg-check" viewBox="0 0 16 16">
            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
            <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
            </svg>`;
       }
     }
   }
+}
+
+function removeUser(j, i) {
+  selectUser.splice(j, 1);
+  console.log('removed', selectUser)
+  document.getElementById(`checked_${i}`).innerHTML = ''
 }
 
 /**
